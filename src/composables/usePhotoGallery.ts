e18ec88bb94958ffd7d22f0a9c5a565e82b1a59d
banzak1 <leosantanabr@gmail.com>
2022-10-20 11:35:52 -0300

    Cirando função para salvar fotos no filesystem

--- src/composables/usePhotoGallery.ts
+++ src/composables/usePhotoGallery.ts
@@ -12,14 +12,47 @@ export function usePhotoGallery() {
         source: CameraSource.Camera,
         quality: 100,
       });
+
       const fileName = new Date().getTime() + '.jpeg';
-      const savedFileImage = {
-        filepath: fileName,
-        webviewPath: photo.webPath,
-      };
+      const savedFileImage = await savePicture(photo, fileName);
 
       photos.value = [savedFileImage, ...photos.value];
     };
+
+    const convertBlobToBase64 = (blob: Blob) =>
+      new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onerror = reject;
+        reader.onload = () => {
+          resolve(reader.result);
+        };
+        reader.readAsDataURL(blob);
+      });
+
+      const savePicture = async (photo: Photo, fileName: string): Promise<UserPhoto> => {
+        let base64Data: string;
+      
+        // Fetch the photo, read as a blob, then convert to base64 format
+        const response = await fetch(photo.webPath!);
+        const blob = await response.blob();
+        // eslint-disable-next-line prefer-const
+        base64Data = (await convertBlobToBase64(blob)) as string;
+      
+        const savedFile = await Filesystem.writeFile({
+          path: fileName,
+          data: base64Data,
+          directory: Directory.Data,
+        });
+      
+        // Use webPath to display the new image instead of base64 since it's
+        // already loaded into memory
+        return {
+          filepath: fileName,
+          webviewPath: photo.webPath,
+        };
+      };
+
+
   
     return {
         photos,
