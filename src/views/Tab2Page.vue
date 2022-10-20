7f097bdfe06a3ceb1d9613f377f9e4966c71f73a
banzak1 <leosantanabr@gmail.com>
2022-10-20 10:48:17 -0300

    Criando projeto e estilizando aba de fotos

---
+++ src/views/Tab2Page.vue
@@ -0,0 +1,36 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Photo Gallery</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-fab vertical="bottom" horizontal="center" slot="fixed">
+        <ion-fab-button @click="takePhoto()">
+          <ion-icon :icon="camera"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { camera, trash, close } from 'ionicons/icons';
+import {   IonPage, IonHeader, IonFab, IonFabButton, IonIcon, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonImg } from '@ionic/vue';
+
+export default defineComponent({
+  name: 'Tab2Page',
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonFab, IonFabButton, IonIcon, 
+    //IonGrid, IonRow, IonCol, IonImg, 
+  },
+  setup() {
+    return {
+      camera,
+      trash,
+      close
+    }
+  }
+});
+</script>
