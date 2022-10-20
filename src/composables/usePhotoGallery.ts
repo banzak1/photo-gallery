36d3ef8af74f91b7414f05df42163c96267f4984
banzak1 <leosantanabr@gmail.com>
2022-10-20 12:19:32 -0300

    Adicionando configuração para mobile

--- src/composables/usePhotoGallery.ts
+++ src/composables/usePhotoGallery.ts
@@ -2,6 +2,7 @@ import { ref, onMounted, watch } from 'vue';
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Preferences } from '@capacitor/preferences';
+import { isPlatform } from '@ionic/vue';
 
 export function usePhotoGallery() {
     const PHOTO_STORAGE = 'photos';
@@ -33,25 +34,39 @@ export function usePhotoGallery() {
 
       const savePicture = async (photo: Photo, fileName: string): Promise<UserPhoto> => {
         let base64Data: string;
-      
-        // Fetch the photo, read as a blob, then convert to base64 format
-        const response = await fetch(photo.webPath!);
-        const blob = await response.blob();
-        // eslint-disable-next-line prefer-const
-        base64Data = (await convertBlobToBase64(blob)) as string;
-      
+        // "hybrid" will detect mobile - iOS or Android
+        if (isPlatform('hybrid')) {
+          const file = await Filesystem.readFile({
+            path: photo.path!,
+          });
+          base64Data = file.data;
+        } else {
+          // Fetch the photo, read as a blob, then convert to base64 format
+          const response = await fetch(photo.webPath!);
+          const blob = await response.blob();
+          base64Data = (await convertBlobToBase64(blob)) as string;
+        }
         const savedFile = await Filesystem.writeFile({
           path: fileName,
           data: base64Data,
           directory: Directory.Data,
         });
       
-        // Use webPath to display the new image instead of base64 since it's
-        // already loaded into memory
-        return {
-          filepath: fileName,
-          webviewPath: photo.webPath,
-        };
+        if (isPlatform('hybrid')) {
+          // Display the new image by rewriting the 'file://' path to HTTP
+          // Details: https://ionicframework.com/docs/building/webview#file-protocol
+          return {
+            filepath: savedFile.uri,
+            webviewPath: photo.webPath,
+          };
+        } else {
+          // Use webPath to display the new image instead of base64 since it's
+          // already loaded into memory
+          return {
+            filepath: fileName,
+            webviewPath: photo.webPath,
+          };
+        }
       };
 
       const cachePhotos = () => {
@@ -66,19 +81,23 @@ export function usePhotoGallery() {
 
 
       const loadSaved = async () => {
-        const photoList = await Preferences.get({key: PHOTO_STORAGE });
+        const photoList = await Preferences.get({ key: PHOTO_STORAGE });
         const photosInPreferences = photoList.value ? JSON.parse(photoList.value) : [];
-
-        for (const photo of photosInPreferences) {
-          const file = await Filesystem.readFile({
-            path: photo.filepath,
-            directory: Directory.Data,
-          });
-          photo.webviewPath = `data:image/jpeg;base64,${file.data}`;          
+      
+        // If running on the web...
+        if (!isPlatform('hybrid')) {
+          for (const photo of photosInPreferences) {
+            const file = await Filesystem.readFile({
+              path: photo.filepath,
+              directory: Directory.Data,
+            });
+            // Web platform only: Load the photo as base64 data
+            photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+          }
         }
-
+      
         photos.value = photosInPreferences;
-      }
+      };
 
       onMounted(loadSaved);
 
